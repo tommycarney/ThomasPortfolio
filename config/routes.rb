8b5ae5337d8fe87b17f7e72596b0160c6c9da4ca
Thomas Carney <tommycarney@gmail.com>
2017-10-30 13:14:47 +0100

    Adds custom route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 end
