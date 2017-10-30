3f6c502865351b243a96be0e2f7572beb2ff4b73
Thomas Carney <tommycarney@gmail.com>
2017-10-30 10:03:53 +0100

    Edit action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,17 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
   def index
     @portfolio_items = Portfolio.all
   end
 
+  def show
+  end
+
   def new
     @portfolio_item = Portfolio.new
+  end
+
+  def edit
   end
 
   def create
@@ -13,15 +20,27 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
 
   private
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+    end
 
     def portfolio_params
       params.require(:portfolio).permit(:title, :subtitle, :body)
