a9b30a0979fb91d253e09f1548be368e5c019ded
Thomas Carney <thomas@thomascarney.org>
2018-01-03 12:39:24 +0100

    Adds skil spec

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @skill = create(:skill)
+  end
+
+  describe "creation" do
+    it "can be created" do
+      expect(@skill).to be_valid
+    end
+  end
+
+  describe "validations" do
+    it "can't be created without a title" do
+      @skill.title = nil
+      expect(@skill).to_not be_valid
+    end
+    it "can't be created without a percent_utilized field" do
+      @skill.percent_utilized = nil
+      expect(@skill).to_not be_valid
+    end
+  end
 end
