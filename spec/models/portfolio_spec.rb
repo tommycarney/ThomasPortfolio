0a4091b76fcc45aa1df5f4860f33bb3442cb4b36
Thomas Carney <thomas@thomascarney.org>
2018-01-03 12:39:10 +0100

    Adds portfolio spec

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Portfolio, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @portfolio = create(:portfolio)
+  end
+
+  describe "creation" do
+    it "can be created" do
+      expect(@portfolio).to be_valid
+    end
+  end
+
+  describe "validations" do
+    it "can't be created without a title" do
+      @portfolio.title = nil
+      expect(@portfolio).to_not be_valid
+    end
+
+    it "can't be created without a body" do
+      @portfolio.body = nil
+      expect(@portfolio).to_not be_valid
+    end
+
+    it "can't be created without a main_image" do
+      @portfolio.main_image = nil
+      expect(@portfolio).to_not be_valid
+    end
+
+    it "can't be created without a thumb_image" do
+      @portfolio.thumb_image = nil
+      expect(@portfolio).to_not be_valid
+    end
+  end
 end
